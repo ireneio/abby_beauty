2da9b89e276b67aa51f64dd9ff023d449f5ec582
IDEA3C <idea3c@IDEA3CdeMacBook-Pro.local>
2024-08-19 05:09:21 +0800

    feat: 產品系列

--- src/components/client/Logo.tsx
+++ src/components/client/Logo.tsx
@@ -59,7 +59,7 @@ export function Logo({
         alt="logo"
         className='w-[72px] h-[72px] object-contain'
       />
-      <h1 className="text-sm font-semibold tracking-[1.5px] font-['BakudaiMedium']">
+      <h1 className="text-xs md:text-sm font-semibold tracking-[1.5px] font-['BakudaiMedium']">
         克麗緹娜
         <br/>
         艾比美容工作室
