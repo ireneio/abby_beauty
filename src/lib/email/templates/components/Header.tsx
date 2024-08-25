d32003308699de030586cd624cc727af5f0ef0f1
IDEA3C <idea3c@IDEA3CdeMacBook-Pro.local>
2024-08-25 23:56:06 +0800

    feat: 預約體驗課程

--- src/lib/email/templates/components/Header.tsx
+++ src/lib/email/templates/components/Header.tsx
@@ -8,18 +8,18 @@ export default function Header() {
     return (
         <Container className="p-4 bg-[#f5f1eb]">
             <div className="flex items-center">
-            {/* Logo */}
-            <Img
-                src={`${process.env.NEXT_PUBLIC_SITE_URL}/images/logo.png`} // Replace with your logo URL
-                alt="Brand Logo"
-                width={50}
-                height={50}
-                className="mr-3"
-            />
-            {/* Brand Text */}
-            <Text className="text-[#484a49] text-xl font-semibold`">
-                艾比美容工作室
-            </Text>
+                {/* Logo */}
+                <Img
+                    src={`${process.env.NEXT_PUBLIC_SITE_URL}/images/logo.png`} // Replace with your logo URL
+                    alt="Brand Logo"
+                    width={50}
+                    height={50}
+                    className="mr-3"
+                />
+                {/* Brand Text */}
+                <Text className="text-[#484a49] text-lg font-semibold`">
+                    艾比美容工作室
+                </Text>
             </div>
         </Container>
     )
