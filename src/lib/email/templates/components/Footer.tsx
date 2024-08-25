728a2c2f64a4da8d7f0050646a9393531bba5669
IDEA3C <idea3c@IDEA3CdeMacBook-Pro.local>
2024-08-26 00:16:05 +0800

    feat: 預約體驗課程 Email

--- src/lib/email/templates/components/Footer.tsx
+++ src/lib/email/templates/components/Footer.tsx
@@ -9,16 +9,22 @@ export default function Footer() {
     return (
         <Container className="p-4 bg-[#f5f1eb]">
             <div className="flex items-center justify-center gap-4 w-full">
-                <Link href={`${process.env.NEXT_PUBLIC_SITE_URL}`} className="underline text-[#484a49] text-sm flex items-center">
-                    預約體驗課程
+                <Link href={`${process.env.NEXT_PUBLIC_SITE_URL}`}>
+                    <Text className="underline text-[#484a49] text-sm flex items-center">
+                        預約體驗課程
+                    </Text>
                 </Link>
                 <Text className="text-[#484a49] text-sm">|</Text>
-                <Link href={`${process.env.NEXT_PUBLIC_SITE_URL}/classes`} className="underline text-[#484a49] text-sm flex items-center">
-                    課程介紹
+                <Link href={`${process.env.NEXT_PUBLIC_SITE_URL}/classes`}>
+                    <Text className="underline text-[#484a49] text-sm flex items-center">
+                        課程介紹
+                    </Text>
                 </Link>
                 <Text className="text-[#484a49] text-sm">|</Text>
                 <Link href={`${process.env.NEXT_PUBLIC_SITE_URL}/products`} className="underline text-[#484a49] text-sm flex items-center">
-                    產品介紹
+                    <Text className="underline text-[#484a49] text-sm flex items-center">
+                        產品介紹
+                    </Text>
                 </Link>
             </div>
         </Container>
