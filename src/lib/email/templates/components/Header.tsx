2daad6a1d035685eaf04eeacffaabf8ae8ec8ee1
IDEA3C <idea3c@IDEA3CdeMacBook-Pro.local>
2024-08-25 23:56:38 +0800

    feat: 預約體驗課程

--- src/lib/email/templates/components/Header.tsx
+++ src/lib/email/templates/components/Header.tsx
@@ -11,9 +11,9 @@ export default function Header() {
                 {/* Logo */}
                 <Img
                     src={`${process.env.NEXT_PUBLIC_SITE_URL}/images/logo.png`} // Replace with your logo URL
-                    alt="Brand Logo"
-                    width={50}
-                    height={50}
+                    alt="艾比美容工作室"
+                    width={64}
+                    height={64}
                     className="mr-3"
                 />
                 {/* Brand Text */}
