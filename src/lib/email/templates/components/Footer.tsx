6a4976c221b8eb0dbe62a2d19aa0130bac6fe0b7
IDEA3C <idea3c@IDEA3CdeMacBook-Pro.local>
2024-08-26 15:15:35 +0800

    feat: Email 樣式

--- src/lib/email/templates/components/Footer.tsx
+++ src/lib/email/templates/components/Footer.tsx
@@ -7,26 +7,24 @@ import {
 
 export default function Footer() {
     return (
-        <Container className="p-4 bg-[#f5f1eb]">
-            <div className="flex items-center justify-between gap-4 w-full">
-                <Link href={`${process.env.NEXT_PUBLIC_SITE_URL}`}>
-                    <Text className="underline text-[#484a49] text-sm flex items-center">
-                        預約體驗課程
-                    </Text>
-                </Link>
-                <Text className="text-[#484a49] text-sm">|</Text>
-                <Link href={`${process.env.NEXT_PUBLIC_SITE_URL}/classes`}>
-                    <Text className="underline text-[#484a49] text-sm flex items-center">
-                        課程介紹
-                    </Text>
-                </Link>
-                <Text className="text-[#484a49] text-sm">|</Text>
-                <Link href={`${process.env.NEXT_PUBLIC_SITE_URL}/products`} className="underline text-[#484a49] text-sm flex items-center">
-                    <Text className="underline text-[#484a49] text-sm flex items-center">
-                        產品介紹
-                    </Text>
-                </Link>
-            </div>
+        <Container className="p-4 bg-[#f5f1eb] flex items-center justify-between gap-4 w-full">
+            <Link href={`${process.env.NEXT_PUBLIC_SITE_URL}`}>
+                <Text className="underline text-[#484a49] text-sm flex items-center">
+                    預約體驗課程
+                </Text>
+            </Link>
+            <Text className="text-[#484a49] text-sm">|</Text>
+            <Link href={`${process.env.NEXT_PUBLIC_SITE_URL}/classes`}>
+                <Text className="underline text-[#484a49] text-sm flex items-center">
+                    課程介紹
+                </Text>
+            </Link>
+            <Text className="text-[#484a49] text-sm">|</Text>
+            <Link href={`${process.env.NEXT_PUBLIC_SITE_URL}/products`} className="underline text-[#484a49] text-sm flex items-center">
+                <Text className="underline text-[#484a49] text-sm flex items-center">
+                    產品介紹
+                </Text>
+            </Link>
         </Container>
     )
 }
