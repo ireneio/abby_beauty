79ddd7ffb2e40d464de5cb75891c04738b2dc477
IDEA3C <idea3c@IDEA3CdeMacBook-Pro.local>
2024-09-01 23:16:39 +0800

    fix: 手機 chrome 修正嘗試

--- next.config.mjs
+++ next.config.mjs
@@ -10,29 +10,29 @@ const nextConfig = {
     domains: ['winop3gdaoox7usg.public.blob.vercel-storage.com']
   },
   webpack: (config) => {
-    if (config.optimization.splitChunks) {
-      config.optimization.splitChunks.cacheGroups = {
-        ...(config.optimization.splitChunks.cacheGroups || {}),
-        adminPages: {
-          test: /[\\/]pages[\\/]admin[\\/]/,
-          name: 'admin',
-          chunks: 'all',
-          enforce: true,
-        },
-        adminComponents: {
-          test: /[\\/]components[\\/]admin[\\/]/,
-          name: 'admin-components',
-          chunks: 'all',
-          enforce: true,
-        },
-        // clientComponents: {
-        //   test: /[\\/]components[\\/]client[\\/]/,
-        //   name: 'client-components',
-        //   chunks: 'all',
-        //   enforce: true,
-        // },
-      }
-    }
+    // if (config.optimization.splitChunks) {
+    //   config.optimization.splitChunks.cacheGroups = {
+    //     ...(config.optimization.splitChunks.cacheGroups || {}),
+    //     adminPages: {
+    //       test: /[\\/]pages[\\/]admin[\\/]/,
+    //       name: 'admin',
+    //       chunks: 'all',
+    //       enforce: true,
+    //     },
+    //     adminComponents: {
+    //       test: /[\\/]components[\\/]admin[\\/]/,
+    //       name: 'admin-components',
+    //       chunks: 'all',
+    //       enforce: true,
+    //     },
+    //     clientComponents: {
+    //       test: /[\\/]components[\\/]client[\\/]/,
+    //       name: 'client-components',
+    //       chunks: 'all',
+    //       enforce: true,
+    //     },
+    //   }
+    // }
     return config
   }
 };
