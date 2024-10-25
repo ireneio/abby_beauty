11d686903d04cedae06ff42b15343c7cab85c7c7
Brown <idea3c@IDEA3CdeMacBook-Pro.local>
2024-10-25 23:47:51 +0800

    首頁調整

--- src/components/client/LineFloatButton.tsx
+++ src/components/client/LineFloatButton.tsx
@@ -2,7 +2,7 @@ import openLineAtAccount from "@/lib/utils/openLineAtAccount";
 
 export default function LineFloatButton() {
     return (
-        <div className="z-[999] fixed bottom-[64px] right-0 p-2 shadow-md bg-primary md:hidden flex flex-col items-center gap-[2px]">
+        <div className="z-[9999] fixed bottom-[64px] right-0 p-2 shadow-md bg-primary md:hidden flex flex-col items-center gap-[2px]">
             <div className="w-[64px] h-[64px] p-2 rounded-full" onClick={() => openLineAtAccount()}>
                 <img src="/images/logo_line.png" />
             </div>
