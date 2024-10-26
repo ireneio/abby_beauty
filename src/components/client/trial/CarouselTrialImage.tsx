4509179c0fd434cee03533b6dd67e7cce2bd2b97
Brown <idea3c@IDEA3CdeMacBook-Pro.local>
2024-10-26 10:10:59 +0800

    體驗課程 UI 修正

--- src/components/client/trial/CarouselTrialImage.tsx
+++ src/components/client/trial/CarouselTrialImage.tsx
@@ -3,17 +3,16 @@ import Image from "next/image";
 import React, { useRef, useState } from "react";
 import Carousel from "react-multi-carousel";
 
-
 const responsive = {
   desktop: {
     breakpoint: { max: 3000, min: 1024 },
     items: 1,
-    // slidesToSlide: 3 // optional, default to 1.
+    slidesToSlide: 1 // optional, default to 1.
   },
   tablet: {
     breakpoint: { max: 1024, min: 640 },
     items: 1,
-    // slidesToSlide: 2 // optional, default to 1.
+    slidesToSlide: 1 // optional, default to 1.
   },
   mobile: {
     breakpoint: { max: 640, min: 0 },
@@ -32,21 +31,21 @@ export default function CarouselTrialImage({ images }: Props) {
         setCurrentSlide(currentSlide);
     };
 
-    const goToNext = () => {
-        if (carouselRef.current) {
-        carouselRef.current.next();
-        }
-    };
+    // const goToNext = () => {
+    //     if (carouselRef.current) {
+    //     carouselRef.current.next();
+    //     }
+    // };
 
-    const goToPrevious = () => {
-        if (carouselRef.current) {
-        carouselRef.current.previous();
-        }
-    };
+    // const goToPrevious = () => {
+    //     if (carouselRef.current) {
+    //     carouselRef.current.previous();
+    //     }
+    // };
 
     const goToSlide = (index: number) => {
         if (carouselRef.current) {
-        carouselRef.current.goToSlide(index);
+            carouselRef.current.goToSlide(index);
         }
     };
 
@@ -63,7 +62,6 @@ export default function CarouselTrialImage({ images }: Props) {
                 autoPlay={false}
                 autoPlaySpeed={1000}
                 keyBoardControl={true}
-                // customTransition="transform 300ms ease-in-out"
                 customTransition="transform 300ms ease-in-out"
                 transitionDuration={500}
                 containerClass="relative"
@@ -71,7 +69,6 @@ export default function CarouselTrialImage({ images }: Props) {
                 // deviceType={"mobile"}
                 dotListClass=""
                 itemClass=""
-                // renderButtonGroupOutside
                 // renderDotsOutside
                 // partialVisible={true}
                 afterChange={handleAfterChange}
